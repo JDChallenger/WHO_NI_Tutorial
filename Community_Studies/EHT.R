b18f8c08d0cf7c82e781cb48e1dfb5c9d65739cf
JDChallenger <joseph.d.challenger@gmail.com>
2025-02-10 14:06:57 +0000

    Start EHTs

--- Community_Studies/EHT.R
+++ Community_Studies/EHT.R
@@ -1,1 +1,45 @@
 library(ggplot2)
+source('Community_Studies/useful_functions_CS.R')
+
+################################################################################
+#########################   Analysing a dataset   ##############################
+################################################################################
+
+#Here, we will simulate a dataset, and then analyse it.
+# The simulated dataset could be replaced with a real one
+
+#Update function name
+mosdata <- EHT_sim2(n_arms = 9, meanMos = 10)
+str(mosdata)
+head(mosdata)
+
+table(mosdata$net)
+table(mosdata$day)
+
+levels(mosdata$net)
+mosdata$net <- relevel(mosdata$net, 'E2')
+levels(mosdata$net) # view levels again
+
+#Fit regression model. day, hut & sleeper included as fixed effects
+
+fit <- glm(
+  cbind(tot_dead, n - tot_dead) ~ net + day + sleeper + hut, 
+  family = binomial, data = mosdata
+)
+summary(fit)
+
+OR <- exp(coef(summary(fit))['netE5',"Estimate"])
+OR_lower <- exp(coef(summary(fit))['netE5',"Estimate"] - 
+                   1.96*coef(summary(fit))['netE5','Std. Error'])
+OR_upper <- exp(coef(summary(fit))['netE5',"Estimate"] + 
+                   1.96*coef(summary(fit))['netE5','Std. Error'])
+
+FIC_mortality <- sum(mosdata[mosdata$net=='E2',]$tot_dead)/sum(mosdata[mosdata$net=='E2',]$n)
+non_inf_margin <- ((FIC_mortality - 0.07) / (1- (FIC_mortality - 0.07))) / (FIC_mortality / (1- FIC_mortality)) 
+
+OR_lower > non_inf_margin # Is the non-inferiority criterion satisfied? This should return 'TRUE', if it is
+
+
+################################################################################
+####################  Simulation-based power calculation   #####################
+################################################################################
